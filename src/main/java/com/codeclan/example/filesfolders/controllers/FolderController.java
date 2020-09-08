a26778aec371bfef4d53e89176c84d42841e2540
Helena <inger.helena@gmail.com>
2020-09-08 17:02:22 +0100

    get index route for folders working

--- src/main/java/com/codeclan/example/filesfolders/controllers/FolderController.java
+++ src/main/java/com/codeclan/example/filesfolders/controllers/FolderController.java
@@ -1,4 +1,24 @@
 package com.codeclan.example.filesfolders.controllers;
 
+import com.codeclan.example.filesfolders.models.Folder;
+import com.codeclan.example.filesfolders.models.User;
+import com.codeclan.example.filesfolders.repositories.FolderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class FolderController {
+
+    @Autowired
+    FolderRepository folderRepository;
+
+    @GetMapping(value = "/folders")
+    public ResponseEntity<List<Folder>> getAllFolders() {
+        return new ResponseEntity(folderRepository.findAll(), HttpStatus.OK);
+    }
 }
