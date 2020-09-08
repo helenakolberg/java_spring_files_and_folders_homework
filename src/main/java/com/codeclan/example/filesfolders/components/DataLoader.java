89ef7e7391a4c5686394500227a5dd3d3c636423
Helena <inger.helena@gmail.com>
2020-09-08 16:57:34 +0100

    amended DataLoader to save all objects, get index route for users working

--- src/main/java/com/codeclan/example/filesfolders/components/DataLoader.java
+++ src/main/java/com/codeclan/example/filesfolders/components/DataLoader.java
@@ -29,12 +29,22 @@ public class DataLoader implements ApplicationRunner {
     public void run(ApplicationArguments args) {
 
         User helena = new User("Helena");
+        userRepository.save(helena);
+
         Folder javaFolder = new Folder("java_stuff", helena);
+        folderRepository.save(javaFolder);
+
         File helenasJavaFile = new File("pirates", "java", 22, javaFolder);
+        fileRepository.save(helenasJavaFile);
 
         User tim = new User("Tim");
+        userRepository.save(tim);
+
         Folder rubyFolder = new Folder("ruby_things", tim);
+        folderRepository.save(rubyFolder);
+
         File timsRubyFile = new File("cool_ruby_app", "ruby", 24, rubyFolder);
+        fileRepository.save(timsRubyFile);
 
     }
 
--- src/test/java/com/codeclan/example/filesfolders/FilesfoldersApplicationTests.java
+++ src/test/java/com/codeclan/example/filesfolders/FilesfoldersApplicationTests.java
@@ -1,5 +1,8 @@
 package com.codeclan.example.filesfolders;
 
+import com.codeclan.example.filesfolders.models.File;
+import com.codeclan.example.filesfolders.models.Folder;
+import com.codeclan.example.filesfolders.models.User;
 import com.codeclan.example.filesfolders.repositories.FileRepository;
 import com.codeclan.example.filesfolders.repositories.FolderRepository;
 import com.codeclan.example.filesfolders.repositories.UserRepository;
@@ -24,6 +27,16 @@ class FilesfoldersApplicationTests {
 	}
 
 	@Test
+	public void create() {
+		User helena = new User("Helena");
+		userRepository.save(helena);
+
+		Folder javaFolder = new Folder("java_stuff", helena);
+		folderRepository.save(javaFolder);
+
+		File helenasJavaFile = new File("pirates", "java", 22, javaFolder);
+		fileRepository.save(helenasJavaFile);
+	}
 
 
 }
